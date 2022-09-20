ec093219fc78d38218a04068dea72515be29718b
JesseRM <jesserrm@gmail.com>
2022-09-20 16:01:53 -0700

    Create reorder click handler

--- client/src/components/Challenge.js
+++ client/src/components/Challenge.js
@@ -4,6 +4,7 @@ import ItemRow from "./ItemRow";
 export default function Challenge() {
   const [orderAmounts, setOrderAmounts] = useState({});
   const [lowStock, setLowStock] = useState([]);
+  const [reorderCost, setReorderCost] = useState(0);
   
   function handleLowStockClick() {
     fetch("http://localhost:4567/low-stock")
@@ -11,6 +12,22 @@ export default function Challenge() {
       .then((data) => {
         setLowStock(data);
       });
+  }
+
+  function handleReorderClick() {
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(orderAmounts)
+    }
+
+    fetch("http://localhost:4567/restock-cost", options)
+      .then((response) => response.json())
+      .then((data) => {
+        setReorderCost(data.cost);
+      })
   }
 
   return (
@@ -48,12 +65,13 @@ export default function Challenge() {
         </tbody>
       </table>
       {/* TODO: Display total cost returned from the server */}
-      <div>Total Cost: </div>
+      <div>Total Cost: {reorderCost.toFixed(2)}</div>
       {/* 
       TODO: Add event handlers to these buttons that use the Java API to perform their relative actions.
-      */}
+      */
+      }
       <button onClick={handleLowStockClick}>Get Low-Stock Items</button>
-      <button>Determine Re-Order Cost</button>
+      <button onClick={handleReorderClick}>Determine Re-Order Cost</button>
     </>
   );
 }
